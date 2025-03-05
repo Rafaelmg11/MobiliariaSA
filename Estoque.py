c598d05888c792caf1dddf3a603c53577876e5ed
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-03-05 20:18:20 -0300

    up

--- Estoque.py
+++ Estoque.py
@@ -1,5 +1,6 @@
 #DECLARANDO AS LISTAS:
 produtos = []
+quantidade = []
 valorDeCompra = []
 valorDeVenda = []
 fornecedor = []
@@ -7,12 +8,33 @@ fornecedor = []
 #CADASTRO DE PRODUTO:
 cont = int(input("Digite quantos produtos você deseja cadastrar: "))
 
-for 
-produtos.append(input("Digite qual é o produto: "))
-valorDeCompra.append(float (input("Digite qual é o valor de compra desse produto(valor do fornecidor): ")))
-valorDeVenda.append(float (input("Digite o valor de venda desse produto: ")))
-fornecedor.append(input ("Digite qual é o fornecedor: "))
+for i in range(cont):
 
+    produtos.append(input("Digite qual é o produto: "))
+    quantidade.append(input("Digite a quantia para o estoque"))
+    valorDeCompra.append(float (input("Digite qual é o valor de compra desse produto(valor do fornecidor): ")))
+    valorDeVenda.append(float (input("Digite o valor de venda desse produto: ")))
+    fornecedor.append(input ("Digite qual é o fornecedor: "))
+
+#VERIFICANDO SE O USUARIO DESEJA CADASTRAR MAIS PRODUTOS
+inf = input("Deseja cadastrar mais produtos? ")
+
+while inf == 'sim' or inf == 's':
+
+    cont = int(input("Digite quantos produtos você deseja cadastrar: "))
+
+    for i in range(cont):
+
+        produtos.append(input("Digite qual é o produto: "))
+        quantidade.append(input("Digite a quantia para o estoque"))
+        valorDeCompra.append(float (input("Digite qual é o valor de compra desse produto(valor do fornecidor): ")))
+        valorDeVenda.append(float (input("Digite o valor de venda desse produto: ")))
+        fornecedor.append(input ("Digite qual é o fornecedor: "))
+
+        inf = input("Deseja cadastrar mais produtos? ")
+
+print("FIM CADASTRO")
+print("INCIO EDITAR")
 #EDITAR PRODUTO:
 pesquisarProduto = input("Digite o produto que você deseja pesquisar: ")
 try:
