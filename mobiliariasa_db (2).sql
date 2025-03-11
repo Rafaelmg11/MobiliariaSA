f01bf164dce038c58d2fdb1697722e5a6c633998
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-03-11 19:36:51 -0300

    up

---
+++ mobiliariasa_db (2).sql
@@ -0,0 +1,36 @@
+create database mobiliaria_db
+
+create table produto(
+	codproduto int not null auto_increment,
+    produto text,
+    descricao text,
+    quantidade int,
+    valordecompra float,
+    valordevenda float,
+    fornecedor text,
+    primary key (codproduto)
+
+);
+
+create table fornecedor(
+    idfornecedor int not null auto_increment,
+    nome_fornecedor text,
+    endereco text,
+    telefone text,
+    email text,
+    produto text,
+    primary key (idfornecedor)
+);
+
+create table funcionario(
+    idfuncionario int not null auto_increment,
+    nome text,
+    cargo text,
+    salario text,
+    primary key (idfuncionario)
+);
+
+
+
+# pip install mysql-connector-python
+
